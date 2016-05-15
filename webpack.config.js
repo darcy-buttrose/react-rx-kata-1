637478f43b4950410c912a93d7df2a92e0260023
Darcy Buttrose <darcy.buttrose@gmail.com>
2016-05-15 15:51:24 +0800

    from the beginning

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+    entry: [
+        './app/entry.tsx'
+    ],
+    devtool: "source-map",
+    debug: true,
+    target: "web",
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "[name].js",
+        chunkFilename: "[name].js",
+        sourceMapFilename: "debugging/[file].map",
+        libraryTarget: undefined
+    },
+    resolve: {
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js', '.tsx', '.jsx'],
+        root: path.join(__dirname, "app"),
+        moduleDirectories: ["web_modules", "node_modules"]
+    },
+    module: {
+        loaders: [
+            { test: /\.(ts(x)?)(\?.*)?$/, loader: 'react-hot-loader!babel?presets[]=react,presets[]=es2016!ts-loader' },
+            { test: /\.(css)(\?.*)?$/, loader: 'style-loader!css-loader' },
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery",
+            "root.jQuery": "jquery"
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        }),
+        new webpack.PrefetchPlugin("react"),
+        new webpack.PrefetchPlugin("react/lib/ReactComponentBrowserEnvironment"),
+    ]
+}
